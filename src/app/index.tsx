01cb1d2537c1a4c25be37edc4b26b86fda7effe6
Awsome Aiminaabee <awsomeaiminaabee@Awsomes-MacBook-Pro.local>
2025-10-13 15:27:48 -0700

    init commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,47 @@
-import { Link } from "expo-router";
+import { Button } from "@/components/button";
+import { Link, router } from "expo-router";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function ScreenOne(){
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <Text style={{ fontSize: 24 }}>Home Screen</Text>
 
       {/* Declarative navigation */}
-      <Link href="/details" style={{ color: "#007AFF", marginTop: 20 }}>
+      <Link href="/details/42/details" style={{ color: "#007AFF", marginTop: 20 }}>
         Go to Details
       </Link>
 
       {/* With params */}
-      <Link href={{ pathname: "/details", params: { id: "42" } }}>
+      <Link href={{ pathname: "/details/[id]/details", params: { id: "42" } }}>
         Go to Details (id = 42)
       </Link>
+
+      {/* Programmatic navigation */}
+      <Button
+        label="router.push to Details (id = 99)" 
+        onPress={() => router.push("/details/99/details")} 
+      />
+
+      <Button
+        variant="outline"
+        label="router.push with params" 
+        onPress={() => router.push({ 
+          pathname: "/details/[id]/details", 
+          params: { 
+            id: "123" 
+          } 
+        })
+      } 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { 
-    flex: 1, 
-    gap: 12, 
-    alignItems: "center", 
-    justifyContent: "center" 
+    flex: 1,
+    justifyContent: "center", 
+    alignItems: "center" 
   }
 });
